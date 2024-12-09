d1266a3b2dbc5c73280d515fc97321e6f053c16c
etherian <rianziwalker@gmail.com>
2024-12-09 14:25:37 +0700

    complete the smart contracts by creating a stake function

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -10,6 +10,8 @@ contract Staker {
 
   mapping (address => uint256) public balances;
 
+  event Stake(address indexed user, uint256 amount);
+
   uint256 public constant thresold = 1 ether;
 
   constructor(address exampleExternalContractAddress) {
